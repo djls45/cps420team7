81ac14145776a1a362bba819d46f74ba1d901a59
Jed Schaaf <dscha042@students.bju.edu>
2013-10-07 23:55:35 -0400

    Update DB tables; set settings files to be ignored

--- src/MainForm.cs
+++ src/MainForm.cs
@@ -14,93 +14,6 @@ using System.Collections.Generic;
 
 namespace CheckTracker
 {
-    [Table(Name = "Checks")]
-    public class AddressEntry
-    {
-        //[Key]
-        //[DatabaseGenerated(DatabaseGeneratedOption.None)] // required for non auto-generated PK
-        public int id { get; set; }
-
-        public string FName { get; set; }
-        public string LName { get; set; }
-
-        public string Street { get; set; }
-        public string City { get; set; }
-        public string StateTerritory { get; set; }
-        public string Country { get; set; }
-        public string PostalCode { get; set; }
-
-        public double Amount { get; set; }
-        public string AmountLong { get; set; }
-
-        public String AccountNum { get; set; }
-        public String RoutingNum { get; set; }
-
-    }
-
-    class CheckTrackerContext : DbContext
-    {
-        // Constructor: provides connection string <connStr> to superclass constructor
-        public CheckTrackerContext(string connStr) : base(connStr) { }
-
-        public DbSet<AddressEntry> AddressEntries { get; set; }
-    }
-
-    class AddressEntryDAO
-    {
-        List<AddressEntry> LoadAll()
-        {
-            using (var db = new CheckTrackerContext("server=localhost;database=AddressBook;"))
-            {
-                // Define a LinQ Query
-                var query = from ae in db.AddressEntries
-                            select ae;
-
-                // Execute the query
-                var results = query.ToList();
-                List<AddressEntry> AEList = null;
-
-                foreach (AddressEntry ae in results)
-                    AEList.Add(ae);
-
-                return AEList;
-            }
-        }
-
-        void Update(AddressEntry AE)
-        {
-            using (var db = new CheckTrackerContext("server=localhost;database=AddressBook;"))
-            {
-                var record = db.AddressEntries.Single(x => x.id == AE.id);
-                record.Name = AE.Name;
-                record.Email = AE.Email;
-                db.SaveChanges();
-                return;
-            }
-        }
-
-        void Create(AddressEntry AE)
-        {
-            using (var db = new CheckTrackerContext("server=localhost;database=AddressBook;"))
-            {
-                db.AddressEntries.Add(AE);
-                db.SaveChanges();
-                return;
-            }
-        }
-
-        void Delete(AddressEntry AE)
-        {
-            using (var db = new CheckTrackerContext("server=localhost;database=AddressBook;"))
-            {
-                var record = db.AddressEntries.Single(x => x.id == AE.id);
-                db.AddressEntries.Remove(record);
-                db.SaveChanges();
-                return;
-            }
-        }
-    }
-
     public partial class MainForm : Form
     {
         public MainForm()
@@ -143,4 +56,102 @@ namespace CheckTracker
             }
         }
     }
+
+
+    ////////////////////////////////////////
+    ////////// Data Access Object //////////
+    ////////////////////////////////////////
+
+    [Table(Name = "Checks")]
+    public class Check
+    {
+        //[Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.None)] // required for non auto-generated PK
+        public int id { get; set; }
+
+        public string CheckNum { get; set; }
+        public double Amount { get; set; }
+        public string AmountLong { get; set; }
+        public string AccountNum { get; set; }
+
+        public int Passer { get; set; }
+        public int Address { get; set; }
+        public int Bank { get; set; }
+
+        public bool Deleted { get; set; }
+        public bool Paid { get; set; }
+
+    }
+
+    class CheckTrackerContext : DbContext
+    {
+        // Constructor: provides connection string <connStr> to superclass constructor
+        public CheckTrackerContext(string connStr) : base(connStr) { }
+
+        public DbSet<Check> Checks { get; set; }
+    }
+
+    class AddressEntryDAO
+    {
+        List<Check> LoadAll()
+        {
+            using (var db = new CheckTrackerContext("server=localhost;database=AddressBook;"))
+            {
+                // Define a LinQ Query
+                var query = from ae in db.Checks
+                            select ae;
+
+                // Execute the query
+                var results = query.ToList();
+                List<Check> AEList = null;
+
+                foreach (Check ae in results)
+                    AEList.Add(ae);
+
+                return AEList;
+            }
+        }
+
+        void Update(Check AE)
+        {
+            using (var db = new CheckTrackerContext("server=localhost;database=AddressBook;"))
+            {
+                var record = db.Checks.Single(x => x.id == AE.id);
+                record.CheckNum = AE.CheckNum;
+                record.Amount = AE.Amount;
+                record.AmountLong = AE.AmountLong;
+                record.AccountNum = AE.AccountNum;
+                record.Address = AE.Address;
+                record.Bank = AE.Bank;
+                record.Deleted = AE.Deleted;
+                record.Paid = AE.Paid;
+                record = AE;
+                db.SaveChanges();
+                return;
+            }
+        }
+
+        void Create(Check AE)
+        {
+            using (var db = new CheckTrackerContext("server=localhost;database=AddressBook;"))
+            {
+                db.Checks.Add(AE);
+                db.SaveChanges();
+                return;
+            }
+        }
+
+        void Delete(Check AE)
+        {
+            using (var db = new CheckTrackerContext("server=localhost;database=AddressBook;"))
+            {
+                var record = db.Checks.Single(x => x.id == AE.id);
+                //db.Checks.Remove(record);
+                record.Deleted = true;
+                db.SaveChanges();
+                return;
+            }
+        }
+    }
+
 }
