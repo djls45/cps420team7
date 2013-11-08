4f88b1964be050497e9c31cfdcdfc2cfdb915fbb
Jed Schaaf <dscha042@students.bju.edu>
2013-11-08 10:54:33 -0500

    DAO separated to own file

---
+++ src/DAO.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Linq;
+
+
+namespace CheckTracker
+{
+    ////////////////////////////////////////
+    ////////// Data Access Object //////////
+    ////////////////////////////////////////
+
+    [System.Data.Linq.Mapping.Table(Name = "Checks")]
+    public class Check
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)] // required for non auto-generated PK
+        public Guid id { get; set; }
+
+        public string CheckNum { get; set; }
+        public decimal Amount { get; set; }
+        public string AmountLong { get; set; }
+
+        public Guid Passer { get; set; }
+        [ForeignKey("Passer")]
+        public Passers Passers { get; set; }
+
+        public Guid Address { get; set; }
+        [ForeignKey("Address")]
+        public Addresses Addresses { get; set; }
+        
+        public Guid Account { get; set; }
+        [ForeignKey("Account")]
+        public Accounts Accounts { get; set; }
+
+        public char Status { get; set; }
+
+        public Guid Employee { get; set; }
+        [ForeignKey("Employee")]
+        public Employees Employees { get; set; }
+
+        public DateTime DateEntered { get; set; }
+
+    }
+
+    [System.Data.Linq.Mapping.Table(Name = "Accounts")]
+    public class Accounts
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)] // required for non auto-generated PK
+        public Guid id { get; set; }
+
+        public string AccountNum { get; set; }
+
+        public string RoutingNum { get; set; }
+        [ForeignKey("RoutingNum")]
+        public Banks Banks { get; set; }
+
+        public Guid Owner { get; set; }
+        [ForeignKey("Owner")]
+        public Passers Passers { get; set; }
+
+    }
+
+    [System.Data.Linq.Mapping.Table(Name = "Addresses")]
+    public class Addresses
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)] // required for non auto-generated PK
+        public Guid id { get; set; }
+
+        public string Street { get; set; }
+        public string AptNo { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+        public string State { get; set; }
+
+    }
+
+    [System.Data.Linq.Mapping.Table(Name = "Banks")]
+    public class Banks
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)] // required for non auto-generated PK
+        public string RoutingNum { get; set; }
+
+        public string Name { get; set; }
+        public Guid Location { get; set; }
+        [ForeignKey("Location")]
+        public Addresses Addresses { get; set; }
+
+    }
+
+    [System.Data.Linq.Mapping.Table(Name = "Configurations")]
+    public class Configurations
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)] // required for non auto-generated PK
+        public Guid id { get; set; }
+
+        public double FeeAmt { get; set; }
+
+    }
+
+    [System.Data.Linq.Mapping.Table(Name = "Employees")]
+    public class Employees
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)] // required for non auto-generated PK
+        public Guid id { get; set; }
+
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public char Type { get; set; }
+
+        public Guid Supervisor { get; set; }
+        [ForeignKey("Supervisor")]
+        public Employees Employees { get; set; }
+        
+        public Guid Store { get; set; }
+        [ForeignKey("Store")]
+        public Stores Stores { get; set; }
+
+        public string Login { get; set; }
+        [ForeignKey("Login")]
+        public Logins Logins { get; set; }
+
+    }
+
+    [System.Data.Linq.Mapping.Table(Name = "Forms")]
+    public class Forms
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)] // required for non auto-generated PK
+        public Guid id { get; set; }
+
+        public string Letter1 { get; set; }
+        public string Letter2 { get; set; }
+        public string Letter3 { get; set; }
+        public string Regulations { get; set; }
+        public string Notices { get; set; }
+
+    }
+
+    [System.Data.Linq.Mapping.Table(Name = "Logins")]
+    public class Logins
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)] // required for non auto-generated PK
+        public Guid id { get; set; }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+
+    [System.Data.Linq.Mapping.Table(Name = "Passers")]
+    public class Passers
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)] // required for non auto-generated PK
+        public Guid id { get; set; }
+
+        public string FName { get; set; }
+        public string LName { get; set; }
+
+        public Guid Address { get; set; }
+        [ForeignKey("Address")]
+        public Addresses Addresses { get; set; }
+
+    }
+
+    [System.Data.Linq.Mapping.Table(Name = "Phones")]
+    public class Phones
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)] // required for non auto-generated PK
+        public Guid id { get; set; }
+
+        public string Number { get; set; }
+        
+        public Guid Passer { get; set; }
+        [ForeignKey("Passer")]
+        public Passers Passers { get; set; }
+
+    }
+
+    [System.Data.Linq.Mapping.Table(Name = "Stores")]
+    public class Stores
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)] // required for non auto-generated PK
+        public Guid id { get; set; }
+
+        public string Name { get; set; }
+
+        public Guid Location { get; set; }
+        [ForeignKey("Location")]
+        public Addresses Addresses { get; set; }
+        
+        public Guid Configuration { get; set; }
+        [ForeignKey("Configuration")]
+        public Configurations Configurations { get; set; }
+        
+        public Guid Form { get; set; }
+        [ForeignKey("Form")]
+        public Forms Forms { get; set; }
+
+    }
+
+    class CheckTrackerContext : DbContext
+    {
+        // Constructor: provides connection string <connStr> to superclass constructor
+        public CheckTrackerContext(string connStr) : base(connStr) { }
+
+        public DbSet<Check> Checks { get; set; }
+    }
+
+    class CheckDAO
+    {
+        public static List<Check> LoadAll()
+        {
+            using (var db = new CheckTrackerContext("server=localhost;database=AddressBook;"))
+            {
+                // Define a LinQ Query
+                var query = from ae in db.Checks
+                            select ae;
+
+                // Execute the query
+                var results = query.ToList();
+                List<Check> AEList = null;
+
+                foreach (Check ae in results)
+                    AEList.Add(ae);
+
+                return AEList;
+            }
+        }
+
+        public static void Update(Check AE)
+        {
+            using (var db = new CheckTrackerContext("server=localhost;database=AddressBook;"))
+            {
+                Check record = db.Checks.Single(x => x.id == AE.id);
+                //record.CheckNum = AE.CheckNum;
+                //record.Amount = AE.Amount;
+                //record.AmountLong = AE.AmountLong;
+                //record.Status = AE.Status;
+                //record.DateEntered = AE.DateEntered;
+
+                //record.Passer = AE.Passer;
+                ////record.Passers = AE.Passers;
+                //record.Address = AE.Address;
+                ////record.Addresses = AE.Addresses;
+                //record.Account = AE.Account;
+                ////record.Accounts = AE.Accounts;
+                //record.Employee = AE.Employee;
+                ////record.Employees = AE.Employees;
+
+                record = AE;
+                db.SaveChanges();
+                return;
+            }
+        }
+
+        public static void Create(Check AE)
+        {
+            using (var db = new CheckTrackerContext("server=localhost;database=AddressBook;"))
+            {
+                db.Checks.Add(AE);
+                db.SaveChanges();
+                return;
+            }
+        }
+
+        public static void Delete(Check AE)
+        {
+            using (var db = new CheckTrackerContext("server=localhost;database=AddressBook;"))
+            {
+                Check record = db.Checks.Single(x => x.id == AE.id);
+                //db.Checks.Remove(record);
+                record.Status = 'D';
+                db.SaveChanges();
+                return;
+            }
+        }
+    }
+}
--- src/MainForm.cs
+++ src/MainForm.cs
@@ -1,15 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
-using System.Data.Linq;
-using System.Data.Entity;
-using System.Data.Linq.Mapping;
 
 namespace CheckTracker
 {
@@ -35,7 +27,11 @@ namespace CheckTracker
             CheckDialog cd = new CheckDialog();
             if (cd.ShowDialog() == DialogResult.OK)
             {
-
+                Addresses Address = new Addresses();
+                Check ch = new Check();
+                //ch.property = textbox.text;
+                ch.Addresses = Address;
+                CheckDAO.Update(ch);
             }
         }
 
@@ -80,236 +76,6 @@ namespace CheckTracker
     }
 
 
-    ////////////////////////////////////////
-    ////////// Data Access Object //////////
-    ////////////////////////////////////////
-
-    [Table(Name = "Checks")]
-    public class Check
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.None)] // required for non auto-generated PK
-        public string id { get; set; }
-
-        public string CheckNum { get; set; }
-        public double Amount { get; set; }
-        public string AmountLong { get; set; }
-
-        public string Passer { get; set; }
-        public string Address { get; set; }
-        public string Account { get; set; }
-
-        public char Status { get; set; }
-
-        public string Employee { get; set; }
-        public DateTime DateEntered { get; set; }
-
-    }
-
-    [Table(Name = "Accounts")]
-    public class Accounts
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.None)] // required for non auto-generated PK
-        public string id { get; set; }
-
-        public string AccountNum { get; set; }
-        public string RoutingNum { get; set; }
-        public string Owner { get; set; }
-
-    }
-
-    [Table(Name = "Addresses")]
-    public class Addresses
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.None)] // required for non auto-generated PK
-        public string id { get; set; }
-
-        public string Street { get; set; }
-        public string AptNo { get; set; }
-        public string City { get; set; }
-        public string Country { get; set; }
-        public string PostalCode { get; set; }
-        public string State { get; set; }
-
-    }
-
-    [Table(Name = "Banks")]
-    public class Banks
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.None)] // required for non auto-generated PK
-        public string RoutingNum { get; set; }
-
-        public string Name { get; set; }
-        public string Location { get; set; }
-
-    }
-
-    [Table(Name = "Configurations")]
-    public class Configurations
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.None)] // required for non auto-generated PK
-        public string id { get; set; }
-
-        public double FeeAmt { get; set; }
-
-    }
-
-    [Table(Name = "Employees")]
-    public class Employees
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.None)] // required for non auto-generated PK
-        public string id { get; set; }
-
-        public string FName { get; set; }
-        public string LName { get; set; }
-        public char Type { get; set; }
-        public string Supervisor { get; set; }
-        public string Store { get; set; }
-        public string Login { get; set; }
-
-    }
-
-    [Table(Name = "Forms")]
-    public class Forms
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.None)] // required for non auto-generated PK
-        public string id { get; set; }
-
-        public string Letter1 { get; set; }
-        public string Letter2 { get; set; }
-        public string Letter3 { get; set; }
-        public string Regulations { get; set; }
-        public string Notices { get; set; }
-
-    }
-
-    [Table(Name = "Logins")]
-    public class Logins
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.None)] // required for non auto-generated PK
-        public string id { get; set; }
-
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public DateTime Date { get; set; }
-
-    }
-
-    [Table(Name = "Passers")]
-    public class Passers
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.None)] // required for non auto-generated PK
-        public string id { get; set; }
-
-        public string FName { get; set; }
-        public string LName { get; set; }
-        public string Address { get; set; }
-
-    }
-
-    [Table(Name = "Phones")]
-    public class Phones
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.None)] // required for non auto-generated PK
-        public string id { get; set; }
-
-        public string Number { get; set; }
-        public string Passer { get; set; }
-
-    }
-
-    [Table(Name = "Stores")]
-    public class Stores
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.None)] // required for non auto-generated PK
-        public string id { get; set; }
-
-        public string Name { get; set; }
-        public string Location { get; set; }
-        public string Configuration { get; set; }
-        public string Forms { get; set; }
-
-    }
-
-    class CheckTrackerContext : DbContext
-    {
-        // Constructor: provides connection string <connStr> to superclass constructor
-        public CheckTrackerContext(string connStr) : base(connStr) { }
-
-        public DbSet<Check> Checks { get; set; }
-    }
-
-    class AddressEntryDAO
-    {
-        List<Check> LoadAll()
-        {
-            using (var db = new CheckTrackerContext("server=localhost;database=AddressBook;"))
-            {
-                // Define a LinQ Query
-                var query = from ae in db.Checks
-                            select ae;
-
-                // Execute the query
-                var results = query.ToList();
-                List<Check> AEList = null;
-
-                foreach (Check ae in results)
-                    AEList.Add(ae);
-
-                return AEList;
-            }
-        }
-
-        void Update(Check AE)
-        {
-            using (var db = new CheckTrackerContext("server=localhost;database=AddressBook;"))
-            {
-                var record = db.Checks.Single(x => x.id == AE.id);
-                record.CheckNum = AE.CheckNum;
-                record.Amount = AE.Amount;
-                record.AmountLong = AE.AmountLong;
-                record.AccountNum = AE.AccountNum;
-                record.Address = AE.Address;
-                record.Bank = AE.Bank;
-                record.Deleted = AE.Deleted;
-                record.Paid = AE.Paid;
-                record = AE;
-                db.SaveChanges();
-                return;
-            }
-        }
-
-        void Create(Check AE)
-        {
-            using (var db = new CheckTrackerContext("server=localhost;database=AddressBook;"))
-            {
-                db.Checks.Add(AE);
-                db.SaveChanges();
-                return;
-            }
-        }
-
-        void Delete(Check AE)
-        {
-            using (var db = new CheckTrackerContext("server=localhost;database=AddressBook;"))
-            {
-                var record = db.Checks.Single(x => x.id == AE.id);
-                //db.Checks.Remove(record);
-                record.Deleted = true;
-                db.SaveChanges();
-                return;
-            }
-        }
-    }
+    
 
 }
